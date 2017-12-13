8c6e6608165de1ea725e3fe1ce0360f548dc04ec
Kowser Kassa <kowserk7@gmail.com>
2017-12-13 15:04:36 -0800

    took out revies

--- koko-caitlin/server.js
+++ koko-caitlin/server.js
@@ -1,8 +1,7 @@
 'use strict';
 
-// REVIEW: There is a package here called body-parser, which is used by the provided POST route. Be sure to install that and save it as a dependency after you create your package.json.
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.use(express.static('./public'));
 
@@ -18,17 +17,13 @@ app.get('/new',(req,res)=>{
   res.sendfile('public/new.html');
 });
 
-
-
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
 app.post('/articles', bodyParser, function(request, response) {
-  // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
 });
-
 
 app.use(function (req,res) {
   res.status(404).send('Error');
