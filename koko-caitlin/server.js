0bb0dca2cd80674d0604046ca6bf6a1f2a7332dc
Caitlin <caitlinsweeneylmt@gmail.com>
2017-12-13 13:24:59 -0800

    got the new.html up on the server

--- koko-caitlin/server.js
+++ koko-caitlin/server.js
@@ -9,10 +9,22 @@ app.use(express.static('./public'));
 app.get('/.data/hackerIpsum.json', function(req,res){
   res.sendgFile('./public/index.html');
 });
+ 
 
 app.listen(3000, function(){
   console.log('sweetcode app listening on port 3000');
 });
+
+
+
+
+app.get('/new',(req,res)=>{
+ 
+    res.sendfile('public/new.html');
+  
+  });
+
+
 
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
