0386b8e4acccd71835f8cd056b2ded0a1ead6350
Koko Kassa <kowserk7@gmail.com>
2018-08-22 13:46:33 -0700

    cleaned up code

--- koko-caitlin/server.js
+++ koko-caitlin/server.js
@@ -2,23 +2,23 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
 app.get('/.data/hackerIpsum.json', function(req,res){
-  res.sendgFile('./public/index.html');
+  res.sendFile('./public/index.html');
 });
 
-app.listen(3000, function(){
-  console.log('sweetcode app listening on port 3000');
+app.listen(PORT, function(){
+  console.log(`App listening on Port: ${PORT}`);
 });
 
 app.get('/new',(req,res)=>{
-  res.sendfile('public/new.html');
+  res.sendFile('./public/new.html');
 });
 
-const bodyParser = require('body-parser').urlencoded({extended: true});
-const PORT = process.env.PORT || 3000;
 
 app.post('/articles', bodyParser, function(request, response) {
   console.log(request.body);
